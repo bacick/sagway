de6c0d29e36ee8d64c5361a06dd49135839e74d5
bacick <eshtin@gmail.com>
2022-04-13 22:14:01 +0300

    install webpack server and htmlplugin, setting this

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-const path = require("path");
+const { resolve } = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js',
+    entry: './src/js/main.js',
     output: {
-        filename: 'main.bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: resolve(__dirname, 'dist'),
+        filename: 'main.[contenthash].js'
+    },
+    plugins: [
+        // new cssExtractPlugin({
+        //     filename: '[name].[contenthash].css'
+        // }),
+        new htmlWebpackPlugin({
+            template: resolve(__dirname,'index.html')
+        }),
+        // new CleanWebpackPlugin(),
+    ],
 }
